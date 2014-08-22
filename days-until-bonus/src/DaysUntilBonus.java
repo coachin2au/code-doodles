ddae13b41b43e6580f94051a3b9ec6630a2031b1
U-IRONMTN\BFrancoeur <BFrancoeur@COPAIM7298.na.imtn.com>
2014-08-22 13:00:53 -0400

    Adding a basic program that will calculate the days unitl I get my bonus.

---
+++ days-until-bonus/src/DaysUntilBonus.java
@@ -0,0 +1,40 @@
+/**
+ * CW33 Programming exercise : This week we'll make a program to calculate days until bonus pay out
+ */
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * @author BFrancoeur
+ *
+ */
+
+
+public class DaysUntilBonus {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+
+		final long MS_IN_A_DAY = 86400000; 
+
+		Calendar defaultPayday = Calendar.getInstance();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+		long delta = 0;
+		
+		/* Current bonus pay day is March 20, 2015 */
+		defaultPayday.set(2015, Calendar.MARCH, 20);
+		
+		delta     = defaultPayday.getTimeInMillis() - Calendar.getInstance().getTimeInMillis();
+		
+		/* Print out the date of the bonus */
+        System.out.println("Today is            : " + dateFormat.format(Calendar.getInstance().getTime()));
+        System.out.println("Your bonus comes on : " + dateFormat.format(defaultPayday.getTime()));
+        System.out.println("Only " + (delta/MS_IN_A_DAY) + " days until you get the bonus.");
+		
+	}
+
+}
