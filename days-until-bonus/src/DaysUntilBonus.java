bde92cb5b0949850577f350c91de149e2302bd1f
bfrancoeur <bill.francoeur@gmail.com>
2014-08-25 16:26:15 -0400

    Fixing formatting on program to remvoe tabs and stuff.

--- days-until-bonus/src/DaysUntilBonus.java
+++ days-until-bonus/src/DaysUntilBonus.java
@@ -7,38 +7,41 @@ import java.util.Calendar;
 
 /**
  * @author BFrancoeur
- *
+ * 
  */
-
 
 public class DaysUntilBonus {
 
-	/**
-	 * @param args
-	 */
-	
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
 
-		/* constant to calculate days from mseconds */
-		final long MS_IN_A_DAY = 86400000; 
+    public static void main(String[] args) {
 
-		/* BFTODO - someday make the payDay a CLI param, here's a default */
-		Calendar defaultPayday      = Calendar.getInstance();
-		/* output format for dates */
-		SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");  
-		/* difference between today and bonus day (in ms) */
-		long delta                  = 0;		
-		
-		/* Current bonus pay day is March 20, 2015 */
-		defaultPayday.set(2015, Calendar.MARCH, 20);
-		
-		delta     = defaultPayday.getTimeInMillis() - Calendar.getInstance().getTimeInMillis();
-		
-		/* Print out the date of the bonus */
-        System.out.println("Today is            : " + dateFormat.format(Calendar.getInstance().getTime()));
-        System.out.println("Your bonus comes on : " + dateFormat.format(defaultPayday.getTime()));
-        System.out.println("You have to wait " + (delta/MS_IN_A_DAY) + " days until you get the bonus.");
-		
-	}
+        /* constant to calculate days from mseconds */
+        final long MS_IN_A_DAY = 86400000;
+
+        /* BFTODO - someday make the payDay a CLI param, here's a default */
+        Calendar defaultPayday = Calendar.getInstance();
+        /* output format for dates */
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        /* difference between today and bonus day (in ms) */
+        long delta = 0;
+
+        /* Current bonus pay day is March 20, 2015 */
+        defaultPayday.set(2015, Calendar.MARCH, 20);
+
+        delta = defaultPayday.getTimeInMillis()
+                - Calendar.getInstance().getTimeInMillis();
+
+        /* Print out the date of the bonus */
+        System.out.println("Today is            : "
+                + dateFormat.format(Calendar.getInstance().getTime()));
+        System.out.println("Your bonus comes on : "
+                + dateFormat.format(defaultPayday.getTime()));
+        System.out.println("You have to wait " + (delta / MS_IN_A_DAY)
+                + " days until you get the bonus.");
+
+    }
 
 }
